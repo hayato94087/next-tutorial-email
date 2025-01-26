c2bb22eba37bb11433eedab3d99ad9f1d6079609
Hayato Akatsuka <75282341+hayato94087@users.noreply.github.com>
2025-01-27 08:32:25 +0900

    メール送信する Server Actions を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { SendEmailDemo } from "@/components/send-email-demo";
+import { env } from "@/env";
 
 export default async function Home() {
+  console.log("Server Components");
+  console.log("RESEND_API_KEY", env.RESEND_API_KEY);
+  console.log("RESEND_DOMAIN", env.RESEND_DOMAIN);
+
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
