27ea32176e9032ad4e2025e757ded5d8f2f4f33d
Hayato Akatsuka <75282341+hayato94087@users.noreply.github.com>
2025-01-28 05:41:00 +0900

    メール送信する Server Actions を作成

--- src/server/actions/email.ts
+++ src/server/actions/email.ts
@@ -1,56 +1,56 @@
 "use server";
 
-import EmailTemplate from "@/emails/welcome-email";
-import { env } from "@/env";
-import { resend } from "@/lib/resend";
+// import EmailTemplate from "@/emails/welcome-email";
+// import { env } from "@/env";
+// import { resend } from "@/lib/resend";
 
-type SendEmailResponse = {
-  data: { id: string } | null;
-  error: Error | null;
-};
+// type SendEmailResponse = {
+//   data: { id: string } | null;
+//   error: Error | null;
+// };
 
-type SendEmailParams = {
-  from?: string;
-  to: string;
-  username: string;
-};
+// type SendEmailParams = {
+//   from?: string;
+//   to: string;
+//   username: string;
+// };
 
-export async function sendWelcomeEmail({
-  from = `Acme <onboarding@${env.RESEND_DOMAIN}>`,
-  to,
-  username,
-}: SendEmailParams): Promise<SendEmailResponse> {
-  console.log("Server Actions");
-  // console.log("RESEND_API_KEY", env.RESEND_API_KEY);
-  // console.log("RESEND_DOMAIN", env.RESEND_DOMAIN);
+// export async function sendWelcomeEmail({
+//   from = `Acme <onboarding@${env.RESEND_DOMAIN}>`,
+//   to,
+//   username,
+// }: SendEmailParams): Promise<SendEmailResponse> {
+//   console.log("Server Actions");
+//   // console.log("RESEND_API_KEY", env.RESEND_API_KEY);
+//   // console.log("RESEND_DOMAIN", env.RESEND_DOMAIN);
 
-  return { data: null, error: null };
-  // // 件名
-  // const subject = "アカウントの作成が完了しました";
-  // // 本文
-  // const react = EmailTemplate({ username });
+//   return { data: null, error: null };
+//   // // 件名
+//   // const subject = "アカウントの作成が完了しました";
+//   // // 本文
+//   // const react = EmailTemplate({ username });
 
-  // try {
-  //   const { data, error } = await resend.emails.send({
-  //     from,
-  //     to,
-  //     subject,
-  //     react,
-  //   });
+//   // try {
+//   //   const { data, error } = await resend.emails.send({
+//   //     from,
+//   //     to,
+//   //     subject,
+//   //     react,
+//   //   });
 
-  //   if (error) {
-  //     return { data: null, error };
-  //   }
+//   //   if (error) {
+//   //     return { data: null, error };
+//   //   }
 
-  //   return { data, error: null };
-  // } catch (error) {
-  //   return {
-  //     data: null,
-  //     error:
-  //       error instanceof Error ? error : new Error("Unknown error occurred"),
-  //   };
-  // }
-}
+//   //   return { data, error: null };
+//   // } catch (error) {
+//   //   return {
+//   //     data: null,
+//   //     error:
+//   //       error instanceof Error ? error : new Error("Unknown error occurred"),
+//   //   };
+//   // }
+// }
 
 export async function hello() {
   console.log("Server Actions");
